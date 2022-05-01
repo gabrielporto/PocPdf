91fe94cdaf9060d0f3e35a0eafd16098df6b5bf0
gabri <gabri@DESKTOP-LTHGNF9>
2022-05-01 02:01:56 -0300

    Add project files.

---
+++ PocPdf/Program.cs
@@ -0,0 +1,13 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+
+string path = $"{Environment.CurrentDirectory}\\sample.pdf";
+PdfReader reader = new PdfReader(path);
+string text = string.Empty;
+for (int page = 1; page <= reader.NumberOfPages; page++)
+{
+    text += PdfTextExtractor.GetTextFromPage(reader, page);
+}
+reader.Close();
+Console.WriteLine(text);
